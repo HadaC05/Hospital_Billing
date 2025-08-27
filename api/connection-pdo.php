8f19e4518b69c597673965172ba27974c4f82fc5
unknown <hada.cubillan.coc@phinmaed.com>
2025-08-27 11:12:28 +0800

    db name

--- api/connection-pdo.php
+++ api/connection-pdo.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
-$dbname = "db_aug26";
+$dbname = "db_hospital";
 
 try {
     $conn = new PDO("mysql:host=$servername; dbname=$dbname", $dbusername, $dbpassword);
